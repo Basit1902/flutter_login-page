d5a499ef2b8fb26467d835ad000779b69c0d25a7
Basit-Iqbal <ganiebasit1902@gmail.com>
2023-12-12 15:48:13 +0530

    Flutter Login page

---
+++ lib/home.dart
@@ -0,0 +1,70 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<ProfileScreen>(
+                  builder: (context) => ProfileScreen(
+                    appBar: AppBar(
+                      title: const Text('User Profile'),
+                    ),
+                    actions: [
+                      SignedOutAction((context) {
+                        Navigator.of(context).pop();
+                      })
+                    ],
+                    children: [
+                      const Divider(),
+                      Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Image.asset('flutterfire_300x.png'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          )
+        ],
+        automaticallyImplyLeading: false,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200, // Set a width constraint for the image
+                height: 200, // Set a height constraint for the image
+                child: Image.asset('dash.png'),
+              ),
+              const SizedBox(height: 20), // Add spacing between elements
+              Text(
+                'Welcome!',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              const SizedBox(height: 20), // Add spacing between elements
+              const SignOutButton(),
+              const SizedBox(height: 20), // Add spacing between elements
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
